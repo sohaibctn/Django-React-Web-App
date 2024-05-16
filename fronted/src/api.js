f510c4e1fdecabad80b6fb7fd61ce73bd32d19cb
sohaib <sohaibmaqbool333@gmail.com>
2024-05-17 01:10:36 +0500

    Update api.js

--- fronted/src/api.js
+++ fronted/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = '/choreo-apis/python-react-web-app/backend-mnp/rest-api-be2/v1',
+const apiUrl = "/choreo-apis/python-react-web-app/backend/rest-api-be2/v1";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
